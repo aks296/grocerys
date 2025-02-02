97c89150e3e4afb04d17c4975d406bdcec7efab6
Aditya Singh <aditya@Adityas-MacBook-Pro.local>
2025-02-02 11:03:51 -0500

    Name Change

--- frontend/grocery-app/capacitor.config.ts
+++ frontend/grocery-app/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.jykra.dwarepe',
-  appName: 'dwarpe',
+  appName: 'doorstep',
   webDir: 'www',
   server: {
     androidScheme: 'https'
